24c98da7e6f8a19a487b1a44759683f118223ce8
houssamhamdaouy <ffree0430@gmail.com>
2026-01-11 13:11:38 +0100

    Premier commit : configuration du projet

--- graph.c
+++ graph.c
@@ -38,26 +38,26 @@ void display(Graphe *g) {
 
 
 void dijkstra(Graphe *g,int s, float dist[]) {
-    int visited[max] = {0};
-    for (int i = 0; i < g->nbv; i++) {
-        dist[i] = inf;
+    int visited[max]={0};
+    for (int i=0;i<g->nbv;i++){
+        dist[i]=inf;
     }
-    dist[s] = 0;
+    dist[s]=0;
 
-    for (int i = 0; i < g->nbv - 1; i++) {
-        float min = inf;
+    for(int i=0;i<g->nbv-1;i++){
+        float min=inf;
         int u;
-        for (int v = 0; v < g->nbv; v++) {
-            if (!visited[v] && dist[v] <= min) {
-                min = dist[v];
-                u = v;
+        for(int v=0;v<g->nbv;v++){
+            if(!visited[v] && dist[v]<= min){
+                min=dist[v];
+                u=v;
             }
         }
-        visited[u] = 1;
-        for (int v = 0; v < g->nbv; v++) {
-            if (!visited[v] && g->mtx[u][v] != inf && dist[u] != inf 
-                && dist[u] + g->mtx[u][v] < dist[v]) {
-                dist[v] = dist[u] + g->mtx[u][v];
+        visited[u]=1;
+        for (int v =0; v <g->nbv;v++){
+            if(!visited[v] && g->mtx[u][v]!= inf &&dist[u]!=inf 
+                && dist[u]+g->mtx[u][v]<dist[v]) {
+                dist[v]=dist[u]+g->mtx[u][v];
             }
         }
     }
